45ea780270cd560cccda5d5cceabdfd8223bfc63
tarikh-cell <tarikhmukhtar@gmail.com>
2021-12-24 17:37:37 +0000

    h

--- workspace/settings.py
+++ workspace/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-$rqn3^w44f0g1wt9h9vraz+q*g0s__8t*)#au*!-2q%o@3gtb*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://.website-workspace.apps.kube.eecs.qmul.ac.uk/']
 
-CSRF_TRUSTED_ORIGINS = ['https://website-workspace.apps.kube.eecs.qmul.ac.uk/']
+CSRF_TRUSTED_ORIGINS = ['https://.website-workspace.apps.kube.eecs.qmul.ac.uk/']
 
 
 # Application definition
